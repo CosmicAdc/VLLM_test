380bc0ef128645b0ab8cd816c8da86478f29d569
Cosmic <andresbloc@hotmail.es>
2024-05-07 20:16:48 -0500

    fastapi

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+from typing import Union
+from fastapi import BackgroundTasks, FastAPI, Request
+from fastapi.responses import JSONResponse, Response, StreamingResponse
+from fastapi import FastAPI
+import langchain
 from langchain.llms import VLLM
 import time
+import uvicorn
 
+app = FastAPI()
 
-llm = VLLM(model="facebook/opt-125m",
+llm = VLLM(model="facebook/opt-125m"",
            trust_remote_code=True,  
            max_new_tokens=50,
            temperature=0.6
 )
 
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-start_time = time.time()
-output = llm("Who is president of US?")
-end_time = time.time()
-latency = end_time - start_time
-print(f"Latency: {latency} seconds")
-print("Generated text:", output)
+
+@app.post("/v1/generateText")
+async def generateText(request: Request) -> Response:
+    request_dict = await request.json()
+    prompt = request_dict.pop("prompt")
+    print(prompt)
+    output = llm(prompt)
+    print("Generated text:", output)
+    ret = {"text": output}
+    return JSONResponse(ret)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5001)
