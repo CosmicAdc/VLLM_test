85d88910bb874c4457a3365d4c96313406911fd4
Cosmic <andresbloc@hotmail.es>
2024-05-07 20:07:09 -0500

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from langchain.llms import VLLM
+import time
+
+
+llm = VLLM(model="tiiuae/falcon-7b-instruct",
+           trust_remote_code=True,  
+           max_new_tokens=50,
+           temperature=0.6
+)
+
+
+start_time = time.time()
+output = llm("Who is president of US?")
+end_time = time.time()
+latency = end_time - start_time
+print(f"Latency: {latency} seconds")
+print("Generated text:", output)
