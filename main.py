a5e5da34a750dee83688bc3553384b6887629ede
Cosmic <andresbloc@hotmail.es>
2024-05-07 20:24:32 -0500

    nginx

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ async def generateText(request: Request) -> Response:
     return JSONResponse(ret)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=5001)
